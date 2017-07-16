e61169cb129cf0086c4e585544ff983eeecc159c
Nate Hero <natehero@gmail.com>
2017-07-16 08:33:51 -0600

    setup mongoose

---
+++ models/todoModel.js
@@ -0,0 +1,14 @@
+var mongood = require('mongoose');
+
+var Schema = mongoose.Schema;
+
+var todoSchema = new Schema({
+  username: String,
+  todo: String,
+  isDone: Boolean,
+  hasAttachment: Boolean
+});
+
+var Todos = mongoose.model('Todos', todoSchema);
+
+module.exports = Todos;
